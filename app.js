3a8622b67fa0b6dfe2c50ef3332eb42ae44ed4c5
johnkagga <johnkagga@gmail.com>
2016-04-01 10:49:18 +0300

    catching errors

--- app.js
+++ app.js
@@ -8,32 +8,46 @@ var root = 'http://188.166.152.118';
 
 // var photos = root + '/comments'; 
 
-var messages = root + '/messages.json';
+var messages = root + '/messages.jso';
 
 var request = http.get(messages, (response) => {
-  console.log(response.statusCode);
-  // consume response body
-  var body = "";
-  
-  response.on('data', function (chuck) {
-      //Add up all the body parts.
-      body += chuck;  
-          
-});
+    console.log(response.statusCode);
+    // consume response body
+    var body = "";
 
-response.on('end', function () {
+    response.on('data', function(chuck) {
+        //Add up all the body parts.
+        body += chuck;
+
+    });
+
+    response.on('end', function() {
+
+if (response.statusCode === 200) {
     
-        var json = JSON.parse(body);
-    
-        console.dir(json);   
-        
+        try {
+            var json = JSON.parse(body);
+        }
+        catch (error) {
+            printError(error);
+        }
+
+        // console.dir(json);   
+
         //tye of the body
-        console.log(typeof json);    
-   });
-   
-  response.resume();
+        console.log(typeof json);
+}
+else{
+    printError( { message: "There was an error retreiving the page " + "(" + http.STATUS_CODES[response.statusCode] + ")"})
+}
+    });
+
+    response.resume();
 });
 
-var error = request.on('error', (e) => {
-  console.log(`Got error: ${e.message}`);
-});
+//Print error
+function printError(error) {
+    console.log("error is: " + error.message);
+}
+
+ request.on('error', printError);

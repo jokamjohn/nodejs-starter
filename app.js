893c00af7cf6bbcb9b65183fd77d788172438a66
johnkagga <johnkagga@gmail.com>
2016-04-01 09:15:33 +0300

    Getting the body chucks

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello John");
+
+var http = require('http');
+
+var username = "chalkers"
+
+var request = http.get('http://www.google.com/index.html', (response) => {
+  console.log(response.statusCode);
+  // consume response body
+  response.on('data', function (chuck) {
+    console.log("BODY: " + chuck)
+})
+  response.resume();
+});
+
+var error = request.on('error', (e) => {
+  console.log(`Got error: ${e.message}`);
+});
